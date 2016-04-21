72a0db707ffeec438e2ea7ea2bd5795fc6a0469f
Keith Moore <keith.m4@gmail.com>
2016-04-21 12:39:46 -0400

    added request to handle http request

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,16 @@
 var express = require('express');
 var router = express.Router();
 var http = require('http');
+var request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 	
 	var apikey = '2664a8f717e9cd0c3d61f2a4990d5df5';
 	var weatherUrl = 'http://api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID=' + apikey;
+	var icon = 'http://openweathermap.org/img/w/';
 	
+	/* THE VANILLA JS WAY
 	var weatherRequest = http.get(weatherUrl, function(response){
 		if (response.statusCode == 200){
 			body = '';
@@ -21,6 +24,16 @@ router.get('/', function(req, res, next) {
 			});
 		}
 	});
+
+	THE REQUEST WAY (uses npm request module) */
+
+	request(weatherUrl, function(error, response, body){
+		if (!error && response.statusCode == 200) {
+			//console.log(body);
+			data = JSON.parse(body);
+			res.render('index', { data: data });
+		}
+	})
 });
 
 module.exports = router;
